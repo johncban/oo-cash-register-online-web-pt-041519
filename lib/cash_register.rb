6a21c6a958d2854cb920dcdcf3eb1f7570cbb048
johncban <johncban@gmail.com>
2019-05-20 06:31:44 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -39,7 +39,7 @@ class CashRegister
   end
 
   def void_last_transaction
-    @total -= @last_transaction # decrease one item of the total item transaction
+    self.total -= self.last_transaction # decrease one item of the total item transaction
   end
 
 end
