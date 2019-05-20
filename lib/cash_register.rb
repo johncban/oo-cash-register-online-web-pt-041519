273c2e10dda40bce64bdc710b4b4e19797258799
johncban <johncban@gmail.com>
2019-05-20 06:32:17 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -24,7 +24,7 @@ class CashRegister
     if @discount == 0
       return "There is no discount to apply."
     end
-    @total -= @total * @discount / 100
+    self.total -= @total * @discount / 100
     return "After the discount, the total comes to $#{@total}."
   end
 
